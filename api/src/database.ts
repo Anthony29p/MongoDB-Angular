bd2e793a0942062f4a517ed5387b1925b1d320a8
anthony29p <anthony.perez@uni.pe>
2022-09-21 23:31:00 -0500

    Front-end (creation)

--- api/src/database.ts
+++ api/src/database.ts
@@ -1,10 +1,10 @@
 require('dotenv').config(); 
 import {connect} from "mongoose";
 
-const {MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT} = process.env;
+const {MONGO_USER, MONGO_PASSWORD, MONGO_HOST, DB_NAME} = process.env;
 
 
-connect(`mongodb://${ MONGO_USER }:${ MONGO_PASSWORD }@${ MONGO_HOST }:${ MONGO_PORT }`,(error) => {
+connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${DB_NAME}`,(error) => {
   error?console.log("********Connection Error********"):console.log("***Connected to MongoDB***")
 })
 
